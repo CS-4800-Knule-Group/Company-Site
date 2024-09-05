c00a02b813e84ba48b85965e2dd3c3978dc26f52
Nathanael Garcia <nathanaelg2099@gmail.com>
2024-09-05 15:35:46 -0700

    commit #2: Update the vite application to have multiple pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import viteLogo from './assets/vite.svg'
 import './App.css'
 
 function App() {
@@ -22,6 +22,8 @@ function App() {
           count is {count}
         </button>
         <p>
+        <a href="/records/">Records</a>
+        <div></div>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
